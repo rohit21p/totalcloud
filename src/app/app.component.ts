09146e0faa72bb65fcfd623ac29474a151771c2c
Rohit <iamrohit.panjwani@gmail.com>
2019-12-03 13:56:25 +0530

    using ngfor for populating avaiable and assigned agents

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'totalcloud';
+  aselected = 0;
+  asselected = 0;
+
+  available = [{
+    name: 'John Doe',
+    img: 'http://cacvs.org/wp-content/uploads/unknown-picture.jpg'
+  }, {
+    name: 'John Doe 2',
+    img: 'http://cacvs.org/wp-content/uploads/unknown-picture.jpg'
+  }];
+
+  assigned = [{
+    name: 'John Doe 3',
+    img: 'http://cacvs.org/wp-content/uploads/unknown-picture.jpg'
+  }];
 }
